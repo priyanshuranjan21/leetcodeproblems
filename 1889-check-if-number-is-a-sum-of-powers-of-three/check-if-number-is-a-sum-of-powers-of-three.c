f97bd70c91d9cdfa7a928aca69993d9701085e54
priyanshuranjan21 <143486229+priyanshuranjan21@users.noreply.github.com>
2025-03-04 13:30:22 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (37.84%) - LeetSync

---
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.c
@@ -0,0 +1,14 @@
+bool checkPowersOfThree(int n) {
+    while(n!=0)
+    {
+        if(n%3>=2)
+        {
+            return false;
+        }
+        else
+        {
+            n/=3;
+        }
+    }
+    return true;
+}
