4b65f94f31796e2bf1f2d3c1f5ac6b6f9de4ec6b
priyanshuranjan21 <143486229+priyanshuranjan21@users.noreply.github.com>
2025-06-20 13:54:09 +0530

    Time: 47 ms (93.03%) | Memory: 38 MB (96.06%) - LeetSync

---
+++ 3754-maximum-manhattan-distance-after-k-changes/maximum-manhattan-distance-after-k-changes.cpp
@@ -0,0 +1,45 @@
+class Solution 
+{
+public:
+    int maxDistance(string s, int k) 
+    {
+        
+        int x = 0;
+        int y = 0;  
+        
+        int ans = 0;
+        
+    
+        for (int i = 0; i < s.size(); ++i) 
+        {  
+            char c = s[i];
+        
+            if (c == 'N')
+            {
+                y++;
+            }      
+            else if (c == 'S')
+            {
+                y--;
+            } 
+            else if (c == 'E')
+            {
+                x++;
+            }
+            else if (c == 'W') 
+            {
+                x--;
+            }
+
+            //to view the sortest path 
+            int real_dist = abs(x) + abs(y);
+            int dist = min(real_dist + 2 * k, i + 1);
+
+            
+            ans = max(ans, dist);
+        }
+
+    
+        return ans;  
+    }
+};
